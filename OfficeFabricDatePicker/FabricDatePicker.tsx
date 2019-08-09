6dd414a2bd381f97437ccc326685ca7795556c1d
Natraj Yegnaraman <natraj.yegnaraman@barhead.com>
2019-08-09 12:07:58 +1000

    1. Add solution 2. Update readme

--- OfficeFabricDatePicker/FabricDatePicker.tsx
+++ OfficeFabricDatePicker/FabricDatePicker.tsx
@@ -1,65 +1,110 @@
 import * as React from "react";
-import { DatePicker, IDatePickerStrings } from 'office-ui-fabric-react/lib/DatePicker';
-import { initializeIcons  } from "office-ui-fabric-react/lib/Icons";
+import {
+  DatePicker,
+  IDatePickerStrings
+} from "office-ui-fabric-react/lib/DatePicker";
+import { initializeIcons } from "office-ui-fabric-react/lib/Icons";
 
 initializeIcons(undefined, { disableWarnings: true });
 
 const DayPickerStrings: IDatePickerStrings = {
-    months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-  
-    shortMonths: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-  
-    days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-  
-    shortDays: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-  
-    goToToday: 'Go to today',
-    prevMonthAriaLabel: 'Go to previous month',
-    nextMonthAriaLabel: 'Go to next month',
-    prevYearAriaLabel: 'Go to previous year',
-    nextYearAriaLabel: 'Go to next year',
-    closeButtonAriaLabel: 'Close date picker'
+  months: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ],
+
+  shortMonths: [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ],
+
+  days: [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ],
+
+  shortDays: ["S", "M", "T", "W", "T", "F", "S"],
+
+  goToToday: "Go to today",
+  prevMonthAriaLabel: "Go to previous month",
+  nextMonthAriaLabel: "Go to next month",
+  prevYearAriaLabel: "Go to previous year",
+  nextYearAriaLabel: "Go to next year",
+  closeButtonAriaLabel: "Close date picker"
 };
 
 export interface IDatePickerProps {
-    inputDateChanged?: (newValue: Date) => void;
-    inputDate?: Date
+  inputDateChanged?: (newValue: Date) => void;
+  inputDate?: Date;
 }
 
-export interface IDatePickerState extends React.ComponentState, IDatePickerProps {
-}
+export interface IDatePickerState
+  extends React.ComponentState,
+    IDatePickerProps {}
 
-export class FabricDatePicker extends React.Component<IDatePickerProps, IDatePickerState> {
-    constructor(props: IDatePickerProps) {
-        super(props);
+export class FabricDatePicker extends React.Component<
+  IDatePickerProps,
+  IDatePickerState
+> {
+  constructor(props: IDatePickerProps) {
+    super(props);
 
-        this.state = {
-          inputDate: props.inputDate
-        };
+    this.state = {
+      inputDate: props.inputDate
+    };
+  }
+
+  render(): JSX.Element {
+    return (
+      <div>
+        <DatePicker
+          strings={DayPickerStrings}
+          firstWeekOfYear={1}
+          placeholder="Select a date..."
+          ariaLabel="Select a date"
+          value={this.state.inputDate}
+          formatDate={date =>
+            `${date.toLocaleDateString(
+              navigator.languages && navigator.languages[0]
+            )}`
+          }
+          showWeekNumbers={true}
+          onSelectDate={this.onSelectDate}
+        />
+      </div>
+    );
+  }
+
+  private onSelectDate = (value: Date): void => {
+    this.setState({ inputDate: value });
+
+    if (this.props.inputDateChanged) {
+      this.props.inputDateChanged(value);
     }
-
-    render(): JSX.Element {
-        return (
-            <div>
-              <DatePicker
-                strings={DayPickerStrings}
-                firstWeekOfYear={1}
-                placeholder="Select a date..."
-                ariaLabel="Select a date"
-                value={this.state.inputDate}
-                formatDate={date=>`${date.toLocaleDateString(navigator.languages && navigator.languages[0])}`}
-                showWeekNumbers={true}
-                onSelectDate={this.onSelectDate}
-              />
-            </div>
-          );
-    }
-
-    private onSelectDate = (value: Date): void => {
-      this.setState({ inputDate: value });
-
-      if (this.props.inputDateChanged) {
-        this.props.inputDateChanged(value);
-      }
-    };    
+  };
 }
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Office UI Fabric Date Picker
+
+The Date Picker that comes out of the box, is a bit clunky, compared to what is available in Office UI Fabric. You can swap the existing DateOnly field, with this custom control.
+
+## Add Custom Control to the field
+
+![alt text](/images/addcontrol.jpg "Add the custom control to the field")
+
+## Configure the control
+
+![alt text](/images/configurecontrol.jpg "Add the custom control to the field")
+
+## Use the control on Unified Interface form
+
+![alt text](/images/usecontrol.jpg "Add the custom control to the field")
